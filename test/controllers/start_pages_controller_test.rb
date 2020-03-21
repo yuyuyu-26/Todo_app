0598bf060bab74c33f871b2cc3d28e85636197ba
ゆゆゆ <yu.yu.yu.bnpg@gmail.com>
2020-03-21 02:57:21 +0000

    Add a full_title helper

--- test/controllers/start_pages_controller_test.rb
+++ test/controllers/start_pages_controller_test.rb
@@ -13,7 +13,7 @@ class StartPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get start_pages_home_url
     assert_response :success
-    assert_select "title", "ホーム | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
