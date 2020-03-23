3682f4dd39a52a5412188b77cc1d39679abfa043
ゆゆゆ <yu.yu.yu.bnpg@gmail.com>
2020-03-23 05:01:58 +0000

    create remember me

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,19 @@ class ActiveSupport::TestCase
   def is_logged_in? #logged_inと混同しないよう
     !session[:user_id].nil?  #ヘルパーメソッドはテストから呼び出せない(current_userじゃなくてsession使用)
   end
+  
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
